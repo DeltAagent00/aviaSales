f253dcc3cc36b58e12fdd0576c94265f524b69ee
Alexandr Zheleznyakov <delta.agent00@gmail.com>
2018-11-30 23:02:27 +0300

    Initial Commit

---
+++ AviaSales/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AviaSales
+//
+//  Created by Alex on 30/11/2018.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AviaSales/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AviaSales
+//
+//  Created by Alex on 30/11/2018.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
